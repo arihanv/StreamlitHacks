e233480295c9dc53bf8386c6b87e4f0745615734
arihanv <arihanvaranasi@gmail.com>
2023-07-26 14:15:27 -0500

    feat: Make a Chat Demo

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 import streamlit as st
-from streamlit.connections import ExperimentalBaseConnection
 from openai_connection.connection import OpenAIConnection
 
 
-conn = st.experimental_connection("openai", type=OpenAIConnection)
+import streamlit as st
 
-st.write(conn.query("Hello, how are you?"))
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+
+st.title("💬 Chatbot Demo")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+if openai_api_key:
+    conn = st.experimental_connection("openai", type=OpenAIConnection, api_key=openai_api_key)
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    st.chat_message("user").write(prompt)
+    try:
+        msg = conn.query(prompt, messages=st.session_state.messages).choices[0].message
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.messages.append(msg)
+        print(st.session_state.messages)
+        st.chat_message("assistant").write(msg.content)
+    except Exception as error:
+        st.info(error)
+        st.stop()
